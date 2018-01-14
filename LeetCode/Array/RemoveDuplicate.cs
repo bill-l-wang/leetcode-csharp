e778a43b067f7d27b6bd167e876f26838dc00da2
bill.wang <bill.wang@ef.com>
2018-01-14 14:04:57 +0800

    initialize

---
+++ LeetCode/Array/RemoveDuplicate.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCode.Array
+{
+    public class RemoveDuplicate
+    {
+        /// <summary>
+        /// Given a	sorted array, remove the duplicates	in place such that	
+        /// appear only once and return the new length
+        /// Do not allocate extra space for another array, you must do this in place with constant memory
+        /// For	example, Given input array A = [1,1,2], Your function should return	length = 2,and A is now	[1,2]
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public int RemoveDuplicates(int[] array, int value)
+        {
+            if (value == 0) return 0;
+
+            var j = 0;
+
+            for(var i = 1; i < array.Length; i++)
+            {
+                if (array[j] != array[i])
+                {
+                    array[++j] = array[i];
+                }
+            }
+
+            return j;
+        }
+
+        /// <summary>
+        /// Follow up for "Remove Duplicates": What	if duplicates are allowed at most twice?
+        /// For	example, Given sorted array	A =	[1,1,1,2,2,3],Your function	should return length = 5, and A	is now [1,1,2,2,3].
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public int RemoveDuplicatesForTwice(int[] array, int value)
+        {
+            if (value == 0) return 0;
+
+            var j = 0;
+            var duplicateTimes = 0;
+
+            for (var i = 1; i < array.Length; i++)
+            {
+                if (array[j] == array[i])
+                {
+                    duplicateTimes++;
+                    if (duplicateTimes < 2)
+                    {
+                        array[++j] = array[i];
+                    }
+                }
+                else
+                {
+                    array[++j] = array[i];
+                    duplicateTimes = 0;
+                }
+            }
+            return j;
+        }
+    }
+}
---
+++ LeetCode/Array/RemoveElement.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace LeetCode.Array
+{
+    /// <summary>
+    /// Given an array and a value, remove all instances of that>value in place and return the new length.
+    /// The order of elements can be changed.It doesn't matter what you leave beyond the new length
+    /// </summary>
+    public class RemoveElement
+    {
+        public int RemoveElements(int[] array, int index, int element)
+        {
+            var j = 0;
+
+            for (var i = 0; i < index; i++)
+            {
+                if (array[i] == element)
+                {
+                    continue;
+                }
+
+                array[j] = array[i];
+                j++;
+            }
+
+            return j;
+        }
+
+        public int RemoveElemets(int[] array, int element)
+        {
+            return array.Count(a => a != element);
+        }
+    }
+}
