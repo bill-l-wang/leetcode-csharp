33cf228d746f5a34a0a36e36ecaf9679ebb0c057
bill.wang <bill.wang@ef.com>
2018-01-15 21:34:07 +0800

    add two sum solution

---
+++ LeetCode.Tests/Array/TwoSumTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using LeetCode.Array;
+using Xunit;
+
+namespace LeetCode.Tests.Array
+{
+    public class TwoSumTest
+    {
+        private const int Target = 9;
+        private const int UnExpectedTarget = 8;
+        private const string InvalidArgumentMessage = "No two sum solution";
+        private readonly int[] _nums = {2, 7, 11, 15};
+        private readonly int[] _expected = {0, 1};
+
+        [Fact]
+        public void Given_an_array_when_sum_succeed_then_return_indices_can_added_up_to_target()
+        {
+            var solution = new TwoSum();
+            var result = solution.Sum(_nums, Target);
+            Assert.Equal(_expected, result);
+        }
+
+        [Fact]
+        public void Given_an_array_when_can_not_find_indices_then_should_throw()
+        {
+            var solution = new TwoSum();
+            var exception = Assert.Throws<ArgumentException>(() => solution.Sum(_nums, UnExpectedTarget));
+            Assert.Equal(InvalidArgumentMessage, exception.Message);
+        }
+    }
+}
---
+++ LeetCode/Array/TwoSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Array
+{
+    /// <summary>
+    /// https://leetcode.com/problems/two-sum/description/
+    /// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+    /// You may assume that each input would have exactly one solution, and you may not use the same element twice
+    /// Given nums = [2, 7, 11, 15], target = 9,
+    /// Because nums[0] + nums[1] = 2 + 7 = 9,
+    /// return [0, 1].
+    /// </summary>
+    public class TwoSum
+    {
+        public int[] Sum(int[] nums, int target)
+        {
+            var dic = nums.Select((value, index) => new {value, index})
+                .ToDictionary(num => num.value, num => num.index);
+
+            for(var i= 0; i< nums.Length; i++)
+            {
+                var complement = target - nums[i];
+                if (dic.ContainsKey(complement) && dic[complement] != i)
+                {
+                    return new[] {i, dic[complement]};
+                }
+            }
+
+            throw new ArgumentException("No two sum solution");
+        }
+    }
+}
