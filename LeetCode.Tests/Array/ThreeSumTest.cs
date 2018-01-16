ccc402adbdf4bbbb62a12c12228cfdb0ef9c271d
bill.wang <bill.wang@ef.com>
2018-01-16 21:52:00 +0800

    add three sum solution

---
+++ LeetCode.Tests/Array/ThreeSumTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using LeetCode.Array;
+using Xunit;
+
+namespace LeetCode.Tests.Array
+{
+    public class ThreeSumTest
+    {
+        private readonly int[] _numbers = {-1, 0, 1, 2, -1, -4};
+        private readonly List<List<int>> _expected = new List<List<int>>
+        {
+            new List<int> {-1, -1, 2},
+            new List<int> {-1, 0, 1}
+        };
+
+        [Fact]
+        public void Given_an_array_when_call_three_sum_then_return_all_unique_triplets_that_sum_is_zero()
+        {
+            var solution = new ThreeSum();
+            var result = solution.Sum(_numbers);
+            Assert.Equal(_expected, result);
+        }
+    }
+}
---
+++ LeetCode/Array/ThreeSum.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Array
+{
+    /// <summary>
+    /// https://leetcode.com/problems/3sum/description/
+    /// Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+    /// Find all unique triplets in the array which gives the sum of zero
+    /// Note: The solution set must not contain duplicate triplets
+    /// For example, given array S = [-1, 0, 1, 2, -1, -4],
+    /// A solution set is:
+    /// [
+    ///  [-1, 0, 1],
+    ///  [-1, -1, 2]
+    /// ]
+    /// </summary>
+    public class ThreeSum
+    {
+        public List<List<int>> Sum(int[] nums)
+        {
+            nums = nums.OrderBy(n => n).ToArray();
+            var results = new List<List<int>>();
+            for (var i = 0; i < nums.Length - 2; i++)
+            {
+                if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+                {
+                    int low = i + 1, high = nums.Length - 1, sum = 0 - nums[i];
+
+                    while (low < high)
+                    {
+                        if (nums[low] + nums[high] == sum)
+                        {
+                            results.Add(new List<int> {nums[i], nums[low], nums[high]});
+                            while (low < high && nums[low] == nums[low + 1]) // skip equal elements to avoid duplicates
+                            {
+                                low++;
+                            }
+                            while (low < high && nums[high] == nums[high - 1])
+                            {
+                                high--;
+                            }
+                            low++;
+                            high--;
+                        }
+                        else if (nums[low] + nums[high] < sum)
+                        {
+                            while (low < high && nums[low] == nums[low + 1])
+                            {
+                                low++;
+                            }
+                            low++;
+                        }
+                        else
+                        {
+                            while (low < high && nums[high] == nums[high - 1])
+                            {
+                                high--;
+                            }
+                            high--;
+                        }
+                    }
+                }
+            }
+
+            return results;
+        }
+    }
+}
