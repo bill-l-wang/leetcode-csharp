badeb079cb6ad483cc6ef164dc18457ae215ad87
bill.wang <bill.wang@ef.com>
2018-02-13 16:50:47 +0800

    add longest valid parentheses

---
+++ LeetCode.Tests/StringTests/ParenthesesTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.String;
+using Xunit;
+
+namespace LeetCode.Tests.StringTests
+{
+    public class ParenthesesTests
+    {
+        [Theory]
+        [InlineData("(()",2)]
+        [InlineData("(()()", 4)]
+        [InlineData(")()())", 4)]
+        [InlineData("())((())", 4)]
+        public void Given_a_string_when_retrieve_then_return_the_length_of_the_longest_valid_parentheses(
+            string s, int expected)
+        {
+            var soution = new Parentheses();
+            var result = soution.LongestValidParentheses(s);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ LeetCode/String/Parentheses.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.String
+{
+    /// <summary>
+    /// https://leetcode.com/problems/longest-valid-parentheses/description/
+    /// Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring
+    /// For "(()", the longest valid parentheses substring is "()", which has length = 2
+    /// Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4
+    /// for example:     ( ) ) ( ( ( ) )
+    /// the index is: -1 0 1 2 3 4 5 6 7
+    /// after calculation, there is no matched parentheses for the index 2,3 )(
+    /// matched: Math.Max(maxans, i - stack.Peek()) 7-3 = 4
+    /// </summary>
+    public class Parentheses
+    {
+        public int LongestValidParentheses(string s)
+        {
+            var maxans = 0;
+            var stack = new Stack<int>();
+            stack.Push(-1);
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (s.Substring(i, 1) == "(")
+                {
+                    stack.Push(i);
+                }
+                else
+                {
+                    stack.Pop();
+                    if (stack.Count == 0)
+                    {
+                        stack.Push(i);
+                    }
+                    else
+                    {
+                        maxans = Math.Max(maxans, i - stack.Peek());
+                    }
+                }
+            }
+
+            return maxans;
+        }
+    }
+}
