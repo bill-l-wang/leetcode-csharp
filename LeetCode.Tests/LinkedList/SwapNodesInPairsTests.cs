746d781ed9cb1a8e59e4e0548c22ba62138d9d79
bill.wang <bill.wang@ef.com>
2018-02-13 13:53:44 +0800

    add swap node in pairs

---
+++ LeetCode.Tests/LinkedList/SwapNodesInPairsTests.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.LinkedList;
+using Xunit;
+
+namespace LeetCode.Tests.LinkedList
+{
+    public class SwapNodesInPairsTests
+    {
+        private readonly ListNode _node1 = new ListNode(1);
+        private readonly ListNode _node2 = new ListNode(2);
+        private readonly ListNode _node3 = new ListNode(3);
+        private readonly ListNode _node4 = new ListNode(4);
+
+        [Fact]
+        public void Given_an_list_when_swap_then_should_swaped_in_pairs()
+        {
+            var head = InitNodeHead();
+            var swapNodes = new SwapNodes();
+            var result = swapNodes.SwapNodesInPairs(head);
+
+            Assert.Equal(2, result.Val);
+            Assert.Equal(1, result.Next.Val);
+            Assert.Equal(4, result.Next.Next.Val);
+            Assert.Equal(3, result.Next.Next.Next.Val);
+        }
+
+        public ListNode InitNodeHead()
+        {
+            _node1.Next = _node2;
+            _node2.Next = _node3;
+            _node3.Next = _node4;
+
+            return _node1;
+        }
+    }
+}
---
+++ LeetCode/LinkedList/SwapNodes.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.LinkedList
+{
+    /// <summary>
+    /// https://leetcode.com/problems/swap-nodes-in-pairs/description/
+    /// Given a linked list, swap every two adjacent nodes and return its head.
+    /// For example,
+    /// Given 1->2->3->4, you should return the list as 2->1->4->3.
+    /// Your algorithm should use only constant space.You may not modify the values in the list, only nodes itself can be changed.
+    /// </summary>
+    public class SwapNodes
+    {
+        public ListNode SwapNodesInPairs(ListNode head)
+        {
+            if (head?.Next == null)
+                return head;
+
+            var tempHead = head;
+            var newHead = head.Next;
+            while (tempHead?.Next != null)
+            {
+                var tmp = tempHead;
+
+                tempHead = tempHead.Next;
+                tmp.Next = tempHead.Next;
+                tempHead.Next = tmp;
+                tempHead = tmp.Next;
+
+                if (tempHead?.Next != null)
+                    tmp.Next = tempHead.Next;
+            }
+
+            return newHead;
+        }
+    }
+}
