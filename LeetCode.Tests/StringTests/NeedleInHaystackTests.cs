f733c29784cd0433eeb52f6d8de6d1df3688d237
bill.wang <bill.wang@ef.com>
2018-02-13 15:25:44 +0800

    add StrStr

---
+++ LeetCode.Tests/StringTests/NeedleInHaystackTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.String;
+using Xunit;
+
+namespace LeetCode.Tests.StringTests
+{
+    public class NeedleInHaystackTests
+    {
+        [Theory]
+        [InlineData("hello", "ll", 2)]
+        [InlineData("aaaaa", "bba", -1)]
+        public void Given_a_needle_when_strstr_then_get_index_of_the_first_occurrence(
+            string hayStack,
+            string needle,
+            int expected)
+        {
+            var solution = new NeedleInHaystack();
+            var result = solution.StrStr(hayStack, needle);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ LeetCode/String/NeedleInHaystack.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.String
+{
+    /// <summary>
+    /// https://leetcode.com/problems/implement-strstr/description/
+    /// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+    /// Example 1: Input: haystack = "hello", needle = "ll" Output: 2
+    /// Example 2: Input: haystack = "aaaaa", needle = "bba" Output: -1
+    /// </summary>
+    public class NeedleInHaystack
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            for (var i = 0; ; i++)
+            {
+                for (var j = 0;; j++)
+                {
+                    if (j == needle.Length)
+                        return i;
+
+                    if (i + j == haystack.Length)
+                        return -1;
+
+                    if (needle.Substring(j, 1) != haystack.Substring(i + j, 1))
+                        break;
+                }
+            }
+        }
+    }
+}
