19aa76d4ca9b8393abb2fb60b591963b5a784e78
bill.wang <bill.wang@ef.com>
2018-02-26 18:07:24 +0800

    Remove node from end

---
+++ LeetCode.Tests/LinkedList/RemoveNodeTests.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.LinkedList;
+using Xunit;
+
+namespace LeetCode.Tests.LinkedList
+{
+    public class RemoveNodeTests
+    {
+        private readonly ListNode _node1 = new ListNode(1);
+        private readonly ListNode _node2 = new ListNode(2);
+        private readonly ListNode _node3 = new ListNode(3);
+        private readonly ListNode _node4 = new ListNode(4);
+        private readonly ListNode _node5 = new ListNode(5);
+
+        [Fact]
+        public void Given_a_linked_list_when_remove_then_the_nth_should_be_removed()
+        {
+            var head = InitNodes();
+
+            var solution = new RemoveNode();
+            var actual = solution.RemoveFromEnd(head, 2);
+
+            Assert.Equal(_node1.Val, actual.Val);
+            Assert.Equal(_node2.Val, _node1.Next.Val);
+            Assert.Equal(_node3.Val, _node1.Next.Next.Val);
+            Assert.Equal(_node5.Val, _node1.Next.Next.Next.Val);
+        }
+
+        private ListNode InitNodes()
+        {
+            _node1.Next = _node2;
+            _node2.Next = _node3;
+            _node3.Next = _node4;
+            _node4.Next = _node5;
+
+            return _node1;
+        }
+    }
+}
---
+++ LeetCode/LinkedList/RemoveNode.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.LinkedList
+{
+    /// <summary>
+    /// Given a linked list, remove the nth node from the end of list and return its head.
+    /// Given linked list: 1->2->3->4->5, and n = 2.
+    /// After removing the second node from the end, the linked list becomes 1->2->3->5.
+    /// Note:
+    ///Given n will always be valid.
+    ///Try to do this in one pass.
+    /// </summary>
+    public class RemoveNode
+    {
+        public ListNode RemoveFromEnd(ListNode head, int index)
+        {
+            var dummy = new ListNode(0) {Next = head};
+            var length = 0;
+            var first = head;
+
+            while (first != null)
+            {
+                length++;
+                first = first.Next;
+            }
+
+            length -= index;
+            first = dummy;
+
+            while (length > 0)
+            {
+                length--;
+                first = first.Next;
+            }
+
+            first.Next = first.Next.Next;
+            return dummy.Next;
+        }
+    }
+}
