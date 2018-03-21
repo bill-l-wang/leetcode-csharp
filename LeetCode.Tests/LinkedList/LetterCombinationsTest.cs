b125584a82d88214cc18a42cf4cc009e5be6baf6
bill.wang <bill.wang@ef.com>
2018-03-21 16:08:45 +0800

    add letter combinations

---
+++ LeetCode.Tests/LinkedList/LetterCombinationsTest.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.LinkedList;
+using Xunit;
+
+namespace LeetCode.Tests.LinkedList
+{
+    public class LetterCombinationsTest
+    {
+        [Theory]
+        [InlineData("23", new[]{ "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" })]
+        [InlineData("12", new[]{ "1a", "1b", "1c" })]
+        public void Given_a_digit_string_when_combine_then_represent_a_mapping_of_letters(string digits, string[] expected)
+        {
+            var solution = new LetterCombinations();
+            var results = solution.Combine(digits);
+            Assert.Equal(expected, results);
+        }
+    }
+}
---
+++ LeetCode/LinkedList/LetterCombinations.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.LinkedList
+{
+    /// <summary>
+    /// https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+    /// Given a digit string, return all possible letter combinations that the number could represent.
+    /// A mapping of digit to letters (just like on the telephone buttons) is given below.
+    /// Input:Digit string "23"
+    /// Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+    /// </summary>
+    public class LetterCombinations
+    {
+        public IEnumerable<string> Combine(string digites)
+        {
+            if(string.IsNullOrEmpty(digites))
+                return Enumerable.Empty<string>();
+
+            var letters = new[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+            var numbers = digites.ToCharArray();
+            var results = new Queue<string>();
+            results.Enqueue("");
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                var buttonLetters = letters[(int) char.GetNumericValue(numbers[i])].ToCharArray();
+                while (results.Peek().Length == i)
+                {
+                    var row = results.Dequeue();
+                    foreach (var letter in buttonLetters)
+                    {
+                        results.Enqueue(row + letter);
+                    }
+                }
+            }
+
+            return results;
+        }
+    }
+}
