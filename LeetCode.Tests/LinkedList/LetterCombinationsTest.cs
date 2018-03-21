a9acda6e3ded2b62cf615bb97d19e89089bcc28b
bill.wang <bill.wang@ef.com>
2018-03-21 18:12:57 +0800

    rename

--- LeetCode.Tests/LinkedList/LetterCombinationsTest.cs
+++ LeetCode.Tests/LinkedList/LetterCombinationsTest.cs
@@ -8,7 +8,7 @@ namespace LeetCode.Tests.LinkedList
         [Theory]
         [InlineData("23", new[]{ "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" })]
         [InlineData("12", new[]{ "1a", "1b", "1c" })]
-        public void Given_a_digit_string_when_combine_then_represent_a_mapping_of_letters(string digits, string[] expected)
+        public void Given_a_digit_string_when_combine_then_represent_a_combinations_of_letters(string digits, string[] expected)
         {
             var solution = new LetterCombinations();
             var results = solution.Combine(digits);
