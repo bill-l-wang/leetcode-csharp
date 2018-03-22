19e316ff5281879d8fae26139aaf963f56b253d1
bill.wang <bill.wang@ef.com>
2018-03-22 16:16:52 +0800

    add empty solution for Inorder traversal

---
+++ LeetCode/Tree/InorderTraversal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Tree
+{
+    public class InorderTraversal
+    {
+        public IEnumerable<int> Traverse(TreeNode root)
+        {
+            return Enumerable.Empty<int>();
+        }
+    }
+}
---
+++ LeetCode/Tree/TreeNode.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Tree
+{
+    public class TreeNode
+    {
+        public TreeNode Left;
+        public TreeNode Right;
+        public int Val;
+
+        public TreeNode(int x)
+        {
+            Val = x;
+        }
+    }
+}
