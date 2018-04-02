aa1088b5e09f21fc133301adfd84addf9b02e35a
bill.wang <bill.wang@ef.com>
2018-04-02 10:48:35 +0800

    add preorder traversal

---
+++ LeetCode/Tree/PreorderTraversal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Tree
+{
+    public class PreorderTraversal
+    {
+        public IEnumerable<int> Preorder(TreeNode root)
+        {
+            if(root == null)
+                return Enumerable.Empty<int>();
+
+            var results = new List<int>();
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                if (node != null)
+                {
+                    results.Add(node.Val);
+                    stack.Push(node.Right);
+                    stack.Push(node.Left);
+                }
+            }
+
+            return results;
+        }
+    }
+}
