5304d809cf41a74f7d6fc032b09baed6de0fb422
bill.wang <bill.wang@ef.com>
2018-04-10 15:42:33 +0800

    add lowest common ancestor Of a binary search tree

---
+++ LeetCode/Tree/LowestCommonAncestorOfBinarySearchTree.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Tree
+{
+    public class LowestCommonAncestorOfBinarySearchTree
+    {
+        public TreeNode GetLowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            while ((root.Val - p.Val) * (root.Val - q.Val) > 0)
+            {
+                root = p.Val < root.Val ? root.Left : root.Right;
+            }
+
+            return root;
+        }
+    }
+}
