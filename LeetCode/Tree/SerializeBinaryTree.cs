7884927f635d7d785b2a7e66f529708c6d63db58
bill.wang <bill.wang@ef.com>
2018-04-16 14:49:42 +0800

    add serialize binary tree

---
+++ LeetCode/Tree/SerializeBinaryTree.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tree
+{
+    /// <summary>
+    /// https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
+    /// </summary>
+    public class SerializeBinaryTree
+    {
+        public string Serialize(TreeNode root)
+        {
+            if (root == null)
+                return string.Empty;
+
+            var sb = new StringBuilder();
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                if (node == null)
+                {
+                    sb.Append("n ");
+                    continue;
+                }
+                sb.Append($"{node.Val} ");
+                queue.Enqueue(node.Left);
+                queue.Enqueue(node.Right);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
