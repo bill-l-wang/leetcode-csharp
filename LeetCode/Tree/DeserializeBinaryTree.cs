3c23ee4ad6ef69b57413de308678f580e75fd666
bill.wang <bill.wang@ef.com>
2018-04-16 22:21:52 +0800

    add deserialize binary tree

---
+++ LeetCode/Tree/DeserializeBinaryTree.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Tree
+{
+    public class DeserializeBinaryTree
+    {
+        public TreeNode Deserialize(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+                return null;
+
+            var queue = new Queue<TreeNode>();
+            var nodes = data.Split(' ');
+            var root = new TreeNode(int.Parse(nodes[0]));
+            queue.Enqueue(root);
+
+            for (var i = 1; i < nodes.Length; i++)
+            {
+                var parent = queue.Dequeue();
+                if (nodes[i] != "n")
+                {
+                    var left = new TreeNode(int.Parse(nodes[i]));
+                    parent.Left = left;
+                    queue.Enqueue(left);
+                }
+
+                if (nodes[++i] != "n")
+                {
+                    var right = new TreeNode(int.Parse(nodes[i]));
+                    parent.Right = right;
+                    queue.Enqueue(right);
+                }
+            }
+
+            return root;
+        }
+    }
+}
