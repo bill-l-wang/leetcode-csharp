c3a5e89094da5484f4e16ac6b6a9d994668e1260
bill.wang <bill.wang@ef.com>
2018-04-17 14:52:11 +0800

    add Lowest Common Ancestor and string difference

---
+++ LeetCode.Tests/StringTests/FindTheDifferenceTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using LeetCode.String;
+
+namespace LeetCode.Tests.StringTests
+{
+    public class FindTheDifferenceTests
+    {
+        [Theory]
+        [InlineData("abcd", "abcde", 'e')]
+        public void Given_two_consist_string_when_click_find_then_return_the_letter_was_added(string s, string t, char expected)
+        {
+            var solution = new FindTheDifference();
+            var actual = solution.FindDifference(s, t);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ LeetCode/Tree/LowestCommonAncestorOfABinaryTree.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Tree
+{
+    public class LowestCommonAncestorOfABinaryTree
+    {
+        public TreeNode GetLowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null || p == null || q == null)
+                return root;
+
+            if (root == p || root == q)
+                return root;
+
+            var left = GetLowestCommonAncestor(root.Left, p, q);
+            var right = GetLowestCommonAncestor(root.Right, p, q);
+
+            if (left == null)
+                return right;
+            else if (right == null)
+                return left;
+            else
+                return root;
+        }
+    }
+}
